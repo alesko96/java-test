dfc8a1627b5534594a940bd080343abb6e7512ee
alesko96 <39317477+alesko96@users.noreply.github.com>
2018-05-16 01:09:09 +0200

    Update CurrencyConverter.java

--- src/main/java/pl/sii/eu/CurrencyConverter.java
+++ src/main/java/pl/sii/eu/CurrencyConverter.java
@@ -9,11 +9,29 @@ import pl.sii.eu.model.Amount;
  * 100 EUR -> 425.18 PLN
  */
 interface CurrencyConverter {
-
+    Amont.Currency euroAmount = Amount.Currency.EUR;
+    Amont.Currency plnAmount = Amount.Currency.PLN;
+       
     double EURO_TO_PLN_RATIO = 4.25181073;
-
+    
+    Amount convertToPln(Amount euroAmount{
+        public Amount pln;
+        pln = Amount*EURO_TO_PLN_RATIO;
+        java.text.DecimalFormat df = new java.text.DecimalFormat();
+        df.setMaximumFractionDigits(PRECISION);
+        df.setMinimumFractionDigits(PRECISION);
+        return (df.format(pln));                         
+    }
+    Amount convertToEur(Amount plnAmount){
+        public Amount eur;
+        eur = Amount/EURO_TO_PLN_RATIO;
+        java.text.DecimalFormat df = new java.text.DecimalFormat();
+        df.setMaximumFractionDigits(PRECISION);
+        df.setMinimumFractionDigits(PRECISION);
+        return (df.format(eur));
+    }
+    
     Amount convertToPln(Amount euroAmount);
-
     Amount convertToEur(Amount plnAmount);
 }
 
